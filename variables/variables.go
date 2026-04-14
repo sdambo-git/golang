8acfe5d59f397d30c5932731cbda4ac9dd445f2c
Shahar Dambo <sdambo@redhat.com>
2026-04-14 13:46:49 +0300

    refactor: remove redundant print statement and fix formatting in output

--- variables/variables.go
+++ variables/variables.go
@@ -12,10 +12,9 @@ func main() {
 	var j int
 	i := Global + AnotherGlobal
 	fmt.Println("initial j value:", j)
-	fmt.Println("j:", j)
 	j = Global
 	// math.abs() requires a float64 argument.
 	// so we typecast the int to float64, and then back to int.
 	k := math.Abs(float64(AnotherGlobal))
-	fmt.Printf("Global=%d, i=%d, j=%d k=%2f .\n", Global, i, j, k)
+	fmt.Printf("Global=%d, i=%d, j=%d k=%.2f .\n", Global, i, j, k)
 }
